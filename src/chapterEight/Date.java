3a2af46684dc3cedcfeb7c823e5702dfe5b087b0
akenz1901 <michael_aka1@icloud.com>
2021-04-11 07:19:25 +0000

    Constructor is now availabe

--- src/chapterEight/Date.java
+++ src/chapterEight/Date.java
@@ -5,13 +5,33 @@ public class Date {
     private int month;
     private int year;
 
+    public Date() {
+        this(0,0,0);
+    }
+    public Date(int day){
+        this(day, 0, 0);
+    }
+    public Date(int day, int month){
+        this(day,month,0);
+    }
+    public Date(int day, int month, int year){
+
+    }
+
     public void setDate(int day, int month, int year) {
-        boolean validateDay = day > 0 & day < 32;
+        validatingDay(day);
+        validatingMonth(month);
+        validatingYear(year);
+    }
+    private void validatingDay(int day){
+        boolean validateDay = day > 0 & day <= 31;
         if(validateDay){ this.day = day; }
-
-        boolean validateMonth = month > 0 & day <= 12;
+    }
+    private void validatingMonth(int month){
+        boolean validateMonth = month > 0 & month <= 12;
         if(validateMonth) { this.month = month; }
-
+    }
+    private void validatingYear(int year){
         boolean validateYear = year >= 2021 & year <= 2023;
         if(validateYear) { this.year = year; }
     }
--- test/tdd/DateClient.java
+++ test/tdd/DateClient.java
@@ -14,36 +14,18 @@ public class DateClient {
     }
 
     @Test
-    void day_canBeCreatedForDate(){
-        nigeriaDate.setDate(6, 6, 2021);
-        assertEquals(6, nigeriaDate.getDay());
-    }
-    @Test
-    void month_canBeCreatedForDate(){
-        nigeriaDate.setDate(6,12, 2021);
+    void date_canBeCreatedForDate() {
+        nigeriaDate.setDate(6, 12, 2021);
         assertEquals(6, nigeriaDate.getDay());
         assertEquals(12, nigeriaDate.getMonth());
         assertEquals(2021, nigeriaDate.getYear());
     }
     @Test
-    void year_canBeCreatedForDate(){
-        nigeriaDate.setDate(24, 7, 2021);
-        assertEquals(2021, nigeriaDate.getYear());
-    }
-    @Test
-    void day_isValidate(){
-        nigeriaDate.setDate(41,0,0);
+    void date_isValidate(){
+        nigeriaDate.setDate(41,67,1995);
         assertEquals(0, nigeriaDate.getDay());
-    }
-    @Test
-    void month_isValidate(){
-        nigeriaDate.setDate(0,50,0);
-        assertEquals(0, nigeriaDate.getDay());
-    }
-    @Test
-    void year_isValidate(){
-        nigeriaDate.setDate(0,0,2037);
-        assertEquals(0, nigeriaDate.getDay());
+        assertEquals(0, nigeriaDate.getMonth());
+        assertEquals(0, nigeriaDate.getYear());
     }
 
 }
