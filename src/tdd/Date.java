23677b0e19e1df9d08297c8b1382987ac6e2e30a
akenz1901 <michael_aka1@icloud.com>
2021-04-12 20:37:55 +0000

    Date can now throw and exception

--- src/tdd/Date.java
+++ src/tdd/Date.java
@@ -1,4 +1,4 @@
-package chapterEight;
+package tdd;
 
 public class Date {
     private int day;
@@ -8,33 +8,41 @@ public class Date {
     public Date() {
         this(0,0,0);
     }
+
     public Date(int day){
         this(day, 0, 0);
     }
+
     public Date(int day, int month){
         this(day,month,0);
     }
     public Date(int day, int month, int year){
-        validatingDay(day);
-        validatingMonth(month);
-        validatingYear(year);
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
     public void setDate(int day, int month, int year) {
+        validateDate(day, month, year);
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    private void validateDate(int day, int month, int year){
         validatingDay(day);
         validatingMonth(month);
         validatingYear(year);
     }
     private void validatingDay(int day){
-        boolean validateDay = day > 0 & day <= 31;
-        if(validateDay){ this.day = day; }
+        boolean validateDay = day > 0 && day <= 31;
+        if(!validateDay)throw new IllegalArgumentException("Pls enter a valid day");
     }
     private void validatingMonth(int month){
-        boolean validateMonth = month > 0 & month <= 12;
-        if(validateMonth) { this.month = month; }
+        boolean validateMonth = month > 0 && month <= 12;
+        if(!validateMonth) throw new IllegalArgumentException("Pls enter a valid month");
     }
     private void validatingYear(int year){
-        boolean validateYear = year >= 2021 & year <= 2023;
-        if(validateYear) { this.year = year; }
+        boolean validateYear = year >= 2021 && year <= 2023;
+        if(!validateYear) throw new IllegalArgumentException("Pls enter a valid year");
     }
     public int getDay() {
         return day;
--- test/tdd/DateClient.java
+++ test/tdd/DateClient.java
@@ -1,10 +1,10 @@
 package tdd;
 
-import chapterEight.Date;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DateClient {
     Date nigeriaDate;
@@ -13,19 +13,18 @@ public class DateClient {
         nigeriaDate = new Date();
     }
     @Test
-    void date_canBeCreatedForDate() {
-        nigeriaDate.setDate(6, 12, 2021);
-        assertEquals(6, nigeriaDate.getDay());
+    void date_canBeSetForDate() {
+        nigeriaDate.setDate(7, 12, 2022);
+        assertEquals(7, nigeriaDate.getDay());
         assertEquals(12, nigeriaDate.getMonth());
-        assertEquals(2021, nigeriaDate.getYear());
+        assertEquals(2022, nigeriaDate.getYear());
     }
     @Test
-    void date_isValidate(){
-        nigeriaDate.setDate(41,67,1995);
-        assertEquals(0, nigeriaDate.getDay());
-        assertEquals(0, nigeriaDate.getMonth());
-        assertEquals(0, nigeriaDate.getYear());
+    void invalidDay_canThrowException(){
+        assertThrows(IllegalArgumentException.class,()->nigeriaDate.setDate(56,2,2021));
     }
+
+    
     @Test
     void date_canBeUsedWithConstructor(){
         nigeriaDate = new Date(29, 12, 2022);
