55e2411ca16309788b072ea543c8a6f0f8e870d3
akenz1901 <michael_aka1@icloud.com>
2021-04-24 16:21:26 +0000

    Generalization Practice

---
+++ test/Inheritance/CarTest.java
@@ -0,0 +1,19 @@
+package Inheritance;
+
+import chapterEight.Car;
+import chapterEight.Vehicle;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class CarTest {
+    @Test
+    void inheritanceChecking(){
+        Car car = new Car();
+        System.out.println("Michael" );
+    }
+
+    public static void main(String[] args) {
+        String[][] listing = new String[][] { { "Book" }, { "Game", "29.99" } }; System.out.println(listing.length + " " + listing[0].length);
+    }
+}
