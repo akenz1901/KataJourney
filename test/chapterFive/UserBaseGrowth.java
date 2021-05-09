d9e2a7204a9eb9dfe5ba3f7d1e37010cbcb65be0
akenz1901 <michael_aka1@icloud.com>
2021-05-09 15:30:34 +0000

    Facebook user's growth summary using compound groth

--- test/chapterFive/UserBaseGrowth.java
+++ test/chapterFive/UserBaseGrowth.java
@@ -1,2 +1,21 @@
-package chapterFive;public class UserBaseGrowth {
+package chapterFive;
+
+public class UserBaseGrowth {
+
+    public static String calculateBaseGrowth(){
+        double currentNumberOfUser = 1000000000.0;
+        double interestRate = 0.04;
+        int numOfMonth = 1;
+        double baseGrowth;
+
+            baseGrowth = currentNumberOfUser * Math.pow(1.0 + interestRate, numOfMonth);
+
+
+
+        return String.format("%d,%.2f", numOfMonth, baseGrowth);
+    }
+
+    public static void main(String[] args) {
+        System.out.print(UserBaseGrowth.calculateBaseGrowth());
+    }
 }
