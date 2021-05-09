939dcd80548d59cf3234f8eb42531e5dc389cfbe
akenz1901 <michael_aka1@icloud.com>
2021-05-09 10:12:06 +0000

    Data Encryption

--- src/chapterFour/DataEncryption.java
+++ src/chapterFour/DataEncryption.java
@@ -3,12 +3,32 @@ package chapterFour;
 public class DataEncryption {
 
 
-    public void takeNumbers(int number){
+    public int takeNumbers(int number){
 
-        int firstNumber = number;
-        int secondNumber = number;
-        int thirdNumber = number;
-        int fourthNum = number;
+        int encryptedDigits;
 
+        int firstNumber = number / 1000;
+        firstNumber += 7;
+        firstNumber %= 10;
+
+        int secondNumber = number / 100;
+        secondNumber += 7;
+        secondNumber %= 10;
+
+        int thirdNumber = number / 10;
+        thirdNumber += 7;
+        thirdNumber %= 10;
+
+        int fourthNum = number / 10;
+        fourthNum += 7;
+        fourthNum %= 10;
+
+        thirdNumber *= 1000;
+        fourthNum *= 100;
+        firstNumber *= 10;
+
+        encryptedDigits = thirdNumber + fourthNum + firstNumber + secondNumber;
+
+        return encryptedDigits;
     }
 }
