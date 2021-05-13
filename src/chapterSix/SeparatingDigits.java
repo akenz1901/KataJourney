318f3c18e21053a8caff61a7ca9f1e1cd97be693
akenz1901 <michael_aka1@icloud.com>
2021-05-13 06:46:15 +0000

    Separating big integers to single

--- src/chapterSix/SeparatingDigits.java
+++ src/chapterSix/SeparatingDigits.java
@@ -1,2 +1,25 @@
-package chapterSix;public class SeparatingDigits {
+package chapterSix;
+
+public class SeparatingDigits {
+    int unit;
+    String result;
+    int r;
+
+    public int getDivision(int a, int b){
+        return a / b;
+    }
+    public int getRemainder(int a, int b){
+        return a % b;
+    }
+    public String display(int number){
+        if (number > 1 && number<99999) {
+            while (number > 0){
+                unit = getRemainder(number, 10);
+                result = unit + " " + result;
+//                number = getDivision(number, 10);
+//                System.out.println(number);
+            }
+        }
+        return result;
+    }
 }
--- test/chapterSix/SeparatingDigitsClient.java
+++ test/chapterSix/SeparatingDigitsClient.java
@@ -1,2 +1,12 @@
-package chapterSix;public class SepratingDigitsClient {
+package chapterSix;
+
+import org.junit.jupiter.api.Test;
+
+public class SeparatingDigitsClient {
+
+    @Test
+    void digitsCanBeSeparated(){
+        SeparatingDigits sp = new SeparatingDigits();
+        System.out.println(sp.display(11000));
+    }
 }
