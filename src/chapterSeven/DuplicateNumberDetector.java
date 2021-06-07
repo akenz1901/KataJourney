81b37b2083f2b8e626a2e530e994f25faccdcf87
akenz1901 <michael_aka1@icloud.com>
2021-06-07 07:01:34 +0100

    Rejection of Duplicate integer not allowed

--- src/chapterSeven/DuplicateNumberDetector.java
+++ src/chapterSeven/DuplicateNumberDetector.java
@@ -1,2 +1,30 @@
-package chapterSeven;public class DuplicateNumberDetector {
+package chapterSeven;
+
+public class DuplicateNumberDetector {
+    private int[] numbers = new int[5];
+
+    private int count = 0;
+    public void inputNumber(int num){
+        for (int number:numbers) {
+            if(number == num) throw new NumberAlreadyExistException("You Just Entered A Duplicate Number");
+        }
+        if(num < 10 || num >100)throw new NumberOutOfBoundException("You Entered Wrong Number Pls Enter Number Within The Range");
+            numbers[count] = num;
+            count++;
+    }
+    public int[] getContainerOfNumbers() {
+        return numbers;
+    }
+
+    private static class NumberOutOfBoundException extends RuntimeException{
+        public NumberOutOfBoundException(String message){
+            super(message);
+        }
+    }
 }
+
+    class NumberAlreadyExistException extends RuntimeException {
+        public NumberAlreadyExistException(String message) {
+            super(message);
+        }
+    }
--- test/chapterSeven/DuplicateNumberDetectorTest.java
+++ test/chapterSeven/DuplicateNumberDetectorTest.java
@@ -1,2 +1,39 @@
-package chapterSeven;public class DuplicateNumberDetectorTest {
+package chapterSeven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class DuplicateNumberDetectorTest {
+
+    @Test
+    void testNumberCanBeInput(){
+        DuplicateNumberDetector numberDetector = new DuplicateNumberDetector();
+
+        numberDetector.inputNumber(10);
+        assertArrayEquals(new int[]{10, 0, 0, 0, 0}, numberDetector.getContainerOfNumbers());
+    }
+
+    @Test
+    void testThatNumberCanBeInputFiveTimes(){
+        DuplicateNumberDetector numbers = new DuplicateNumberDetector();
+        numbers.inputNumber(50);
+        numbers.inputNumber(90);
+        numbers.inputNumber(60);
+        numbers.inputNumber(40);
+        numbers.inputNumber(16);
+
+        assertArrayEquals(new int[]{50, 90, 60, 40, 16}, numbers.getContainerOfNumbers());
+    }
+
+    @Test
+    void duplicateNumber_cannotBeTaken(){
+        DuplicateNumberDetector uniqueNumbers = new DuplicateNumberDetector();
+
+        uniqueNumbers.inputNumber(33);
+        uniqueNumbers.inputNumber(67);
+        uniqueNumbers.inputNumber(33);
+
+        assertArrayEquals(new int[]{33, 67, 0, 0, 0}, uniqueNumbers.getContainerOfNumbers());
+    }
 }
