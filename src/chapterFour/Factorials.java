8449e990219dfbd331b9fd0568351c32ff43cc21
akenz1901 <michael_aka1@icloud.com>
2021-06-16 05:16:19 +0100

    Factorial

---
+++ src/chapterFour/Factorials.java
@@ -0,0 +1,16 @@
+package chapterFour;
+
+public class Factorials {
+
+    void factorialNumbers(int number){
+        for (int i = number-1; i > 0 ; i--) {
+                number *= i;
+        }
+        System.out.println(number);
+    }
+
+    public static void main(String[] args) {
+        Factorials factor = new Factorials();
+        factor.factorialNumbers(4);
+    }
+}
