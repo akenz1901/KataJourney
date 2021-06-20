d51448e859d2fbca07d44995e2c956b4040c855f
akenz1901 <michael_aka1@icloud.com>
2021-06-20 08:31:26 +0100

    Student test

--- test/chapterEight/StudentTest.java
+++ test/chapterEight/StudentTest.java
@@ -12,4 +12,21 @@ public class StudentTest {
 
         System.out.println(student);
     }
+    static double percent; //1
+    int offset = 10, base= 50; //2
+    public static double calc(double value) {
+        var coupon = 0;
+                var offset = 0;
+        var base = 0; //3
+        if(percent <10){ //4
+            coupon = 15;
+            offset = 20;
+            base = 10;
+        }
+        return coupon*offset*base*value/100; //5
+    }
+    public static void main(String[] args) {
+        System.out.println(calc(100));
+    }
+
 }
