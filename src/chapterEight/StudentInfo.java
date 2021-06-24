7ac44af4c3b18e0beb913a86114c614d62acafdb
akenz1901 <michael_aka1@icloud.com>
2021-06-24 09:53:08 +0100

    Modified Student Info

--- src/chapterEight/StudentInfo.java
+++ src/chapterEight/StudentInfo.java
@@ -6,14 +6,21 @@ public class StudentInfo {
     private final String firstName;
     private final String lastName;
     private final Date dateOfBirth;
+    private static int count;
 
     public StudentInfo(String firstName, String lastName, Date dateOfBirth){
         this.dateOfBirth = dateOfBirth;
         this.firstName = firstName;
         this.lastName = lastName;
+        ++count;
     }
 
     public String toString(){
         return String.format("FullName: %s %s%n Date Of Birth: %s", firstName, lastName, dateOfBirth);
     }
+
+    public static int getNumberOfEmployee(){
+        return count;
+    }
+
 }
--- test/chapterEight/StudentTest.java
+++ test/chapterEight/StudentTest.java
@@ -6,27 +6,30 @@ import tdd.Date;
 public class StudentTest {
 
     @Test
-    void studentInfoTest(){
-        Date date = new Date(24,3, 2013);
+    void studentInfoTest() {
+        Date date = new Date(24, 3, 2013);
         StudentInfo student = new StudentInfo("Mercy", "Olashile", date);
 
         System.out.println(student);
     }
-    static double percent; //1
-    int offset = 10, base= 50; //2
-    public static double calc(double value) {
-        var coupon = 0;
-                var offset = 0;
-        var base = 0; //3
-        if(percent <10){ //4
-            coupon = 15;
-            offset = 20;
-            base = 10;
-        }
-        return coupon*offset*base*value/100; //5
-    }
+
     public static void main(String[] args) {
         System.out.println(calc(100));
     }
 
+
+    static double percent;
+    int offset = 10, base = 50;
+
+    public static double calc(double value) {
+        var coupon = 0;
+        var offset = 0;
+        var base = 0;
+        if (percent < 10) {
+            coupon = 15;
+            offset = 20;
+            base = 10;
+        }
+        return coupon * offset * base * value / 100;
+    }
 }
