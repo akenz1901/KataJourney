9c0a50c90427e0adad3a5f016a9a3614c064f9c6
akenz1901 <michael_aka1@icloud.com>
2021-08-14 17:35:14 +0100

    Files and Stream Practice

---
+++ src/chapterFiften/FileAndDirectoryInformations.java
@@ -0,0 +1,39 @@
+package chapterFiften;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileAndDirectoryInformations {
+
+    static String checkPath(String tempPaths) throws IOException {
+
+        Path path = Paths.get(tempPaths);
+
+        if(Files.exists(path)){
+            System.out.printf("%n%s", path.getFileName());
+            System.out.printf("%s a directory%n", Files.isDirectory(path)? "Is":"Is not" );
+            System.out.printf("%s an absolute path%n", path.isAbsolute() ? "Is":"Is not");
+            System.out.printf("Last modified: s%n%", Files.getLastModifiedTime(path));
+            System.out.printf("Size: %s%n", Files.size(path));
+            System.out.printf("Path: %s%n", path);
+            System.out.printf("Absolute path: %s%n", path.toAbsolutePath());
+
+            if (Files.isDirectory(path)){
+                System.out.printf("%nDirectory contents:%n");
+
+                DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path);
+
+                for (Path newPath:directoryStream) {
+                    System.out.println(newPath);
+                }
+            }
+            return String.format("%n%s", Files.exists(path));
+        }
+        else
+         return String.format("%s exist", Files.exists(path) ? path + "does": path + "does not");
+    }
+
+}
