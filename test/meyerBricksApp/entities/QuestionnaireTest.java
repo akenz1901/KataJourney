c5e95e54f59800431e1fc0d4a6aa6ae0703d5384
akenz1901 <michael_aka1@icloud.com>
2021-08-25 13:13:14 +0100

    QuestionnaireTest

--- test/meyerBricksApp/entities/QuestionnaireTest.java
+++ test/meyerBricksApp/entities/QuestionnaireTest.java
@@ -1,4 +1,47 @@
+package meyerBricksApp.entities;
+
+import meyerBricksApp.DataStore.ChoiceType;
+import meyerBricksApp.DataStore.services.QuestionService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class QuestionnaireTest {
-  
+    Questionnaire question;
+    @BeforeEach
+    void setUp(){
+        question = new Questionnaire();
+    }
+    @AfterEach
+    void tearDown(){}
+
+    @Test
+    void questionnaireNotNull(){
+        assertNotNull(question);
+    }
+    @Test
+    void checkQuestionA_andBNotEmpty(){
+        assertFalse(question.getQuestionAs().isEmpty());
+        assertFalse(question.getQuestionBs().isEmpty());
+    }
+    @Test
+    void testThatTwentyQuestionsAreThereInEachQuestionRepo(){
+        assertEquals(20, question.getQuestionAs().size());
+        assertEquals(20, question.getQuestionBs().size());
+    }
+    @Test
+    void compareAndAllQuestionsInSectionA_AreUnique(){
+        for (int i = 0; i < QuestionService.getSetA().length; i++) {
+            assertEquals(QuestionService.getSetA()[i], question.getQuestionAs().get(i+1));
+        }
+    }
+    @Test
+    void compareAndAllQuestionsInSectionB_AreUnique(){
+        for (int i = 0; i < QuestionService.getSetB().length; i++) {
+            System.out.println(i);
+            assertEquals(QuestionService.getSetB()[i], question.getQuestionBs().get(i+1));
+        }
+    }
 }
