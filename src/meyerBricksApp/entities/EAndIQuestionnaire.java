410ad51740fc4600162b1db83b91f6ac930beb02
akenz1901 <michael_aka1@icloud.com>
2021-08-28 00:05:42 +0100

    Questionnaire EandI means Extrovert and Introvert Questionnaire it's a child to parent questionnaire

--- src/meyerBricksApp/entities/EAndIQuestionnaire.java
+++ src/meyerBricksApp/entities/EAndIQuestionnaire.java
@@ -1,2 +1,38 @@
-package meyerBricksApp.entities;public class EAndIQuestionnaire {
+package meyerBricksApp.entities;
+
+import meyerBricksApp.DataStore.services.QuestionService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EAndIQuestionnaire extends Questionnaire{
+    Map<Integer, String> questionAs = new HashMap<>(5);
+    Map<Integer, String> questionBs = new HashMap<>(5);
+
+    public EAndIQuestionnaire(){
+        setQuestionInA();
+        setQuestionInB();
+    }
+    @Override
+    protected void setQuestionInA() {
+        for (int i = 0; i < questionAs.size(); i++) {
+            questionAs.put(QuestionService.generateNumber()+1, QuestionService.getSetA()[i]);
+        }
+    }
+
+    @Override
+    protected void setQuestionInB() {
+        QuestionService.resetControl();
+        for (int i = 0; i < questionBs.size(); i++) {
+            questionBs.put(QuestionService.generateNumber()+1, QuestionService.getSetA()[i]);
+        }
+    }
+
+    public Map<Integer, String> getQuestionA() {
+        return questionAs;
+    }
+
+    public Map<Integer, String> getQuestionB() {
+        return questionBs;
+    }
 }
