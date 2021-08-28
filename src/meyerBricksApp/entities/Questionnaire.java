757f507472742d707fb5fb27eafa3a8bc738abbf
akenz1901 <michael_aka1@icloud.com>
2021-08-28 16:37:58 +0100

    Questionnaire is absolutely refactored

--- src/meyerBricksApp/entities/Questionnaire.java
+++ src/meyerBricksApp/entities/Questionnaire.java
@@ -7,41 +7,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Questionnaire {
-    private static Map<Integer, String> questionAs = new HashMap<>();
-    private static Map<Integer, String> questionBs = new HashMap<>();
-    private ChoiceType choice;
+    private final Map<Integer, String> questionAs = new HashMap<>();
+    private final Map<Integer, String> questionBs = new HashMap<>();
 
-    public Questionnaire(){
-        setQuestionInA();
-        setQuestionInB();
-    }
-    protected void setQuestionInA() {
-        for (int i = 0; i < QuestionService.getSetA().length; i++) {
-            Questionnaire.questionAs.put(QuestionService.generateNumber()+1, QuestionService.getSetA()[i]);
-        }
-    }
-    protected void setQuestionInB() {
-        QuestionService.resetControl();
-        for (int i = 0; i < QuestionService.getSetB().length; i++) {
-            questionBs.put(QuestionService.generateNumber()+1, QuestionService.getSetB()[i]);
-        }
-    }
-    private static void setQuestionAs(Map<Integer, String> questionAs) {
-        Questionnaire.questionAs = questionAs;
-    }
-
-    private void setQuestionBs(Map<Integer, String> questionBs) {
-        Questionnaire.questionBs = questionBs;
-    }
+    public abstract  void setQuestionInA();
+    public abstract void setQuestionInB();
 
     public Map<Integer, String> getQuestionAs() {
         return questionAs;
     }
     public Map<Integer, String> getQuestionBs() {
         return questionBs;
-    }
-
-    private void setQuestions(Map<Integer, String> questions) {
-        questionAs = questions;
     }
 }
