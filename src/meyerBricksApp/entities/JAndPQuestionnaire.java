6138f59f47d4d722930265fc28a178367d216344
akenz1901 <michael_aka1@icloud.com>
2021-08-28 16:37:04 +0100

    Judging and Perceiving Questionnaire nwo being refactore

--- src/meyerBricksApp/entities/JAndPQuestionnaire.java
+++ src/meyerBricksApp/entities/JAndPQuestionnaire.java
@@ -1,2 +1,24 @@
-package meyerBricksApp.entities;public class JAndPQuestionnaire {
+package meyerBricksApp.entities;
+
+import meyerBricksApp.DataStore.services.QuestionService;
+
+public class JAndPQuestionnaire extends Questionnaire{
+
+    @Override
+    public void setQuestionInA() {
+        int count = 1;
+        for (int i = 5; i < 10; i++) {
+            super.getQuestionAs().put(count, QuestionService.getSetA()[i]);
+            count++;
+        }
+    }
+
+    @Override
+    public void setQuestionInB() {
+        int count = 1;
+        for (int i = 5; i < 10; i++) {
+            super.getQuestionBs().put(count, QuestionService.getSetB()[i]);
+            count++;
+        }
+    }
 }
