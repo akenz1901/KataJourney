b3bf53a85fa426f186e24079e9cadb985bc88e14
akenz1901 <michael_aka1@icloud.com>
2021-08-29 23:22:03 +0100

    Refactored Sensing and Intuition

--- src/meyerBricksApp/entities/JAndPQuestionnaire.java
+++ src/meyerBricksApp/entities/JAndPQuestionnaire.java
@@ -1,24 +1,13 @@
 package meyerBricksApp.entities;
 
-import meyerBricksApp.DataStore.services.QuestionService;
-
-public class JAndPQuestionnaire extends Questionnaire{
-
+public class JAndIQuestionnaire extends Questionnaire{
     @Override
     public void setQuestionInA() {
-        int count = 1;
-        for (int i = 5; i < 10; i++) {
-            super.getQuestionAs().put(count, QuestionService.getSetA()[i]);
-            count++;
-        }
+
     }
 
     @Override
     public void setQuestionInB() {
-        int count = 1;
-        for (int i = 5; i < 10; i++) {
-            super.getQuestionBs().put(count, QuestionService.getSetB()[i]);
-            count++;
-        }
+
     }
 }
