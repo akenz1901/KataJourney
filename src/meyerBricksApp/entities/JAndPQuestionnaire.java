29c2f5db5499fc89573f64f04cb348309ecd690c
akenz1901 <michael_aka1@icloud.com>
2021-08-29 23:26:04 +0100

    Question A had been added to the question repo

--- src/meyerBricksApp/entities/JAndPQuestionnaire.java
+++ src/meyerBricksApp/entities/JAndPQuestionnaire.java
@@ -1,9 +1,15 @@
 package meyerBricksApp.entities;
 
-public class JAndIQuestionnaire extends Questionnaire{
+import meyerBricksApp.DataStore.services.QuestionService;
+
+public class JAndPQuestionnaire extends Questionnaire{
     @Override
     public void setQuestionInA() {
-
+        int count = 1;
+        for (int i = 10; i < 15; i++) {
+            super.getQuestionAs().put(count, QuestionService.getSetA()[i]);
+            count++;
+        }
     }
 
     @Override
