3582b58e9da69e66648ab8f2eb6db3a953c2f162
akenz1901 <michael_aka1@icloud.com>
2021-08-30 22:32:17 +0100

    Last question added

--- src/meyerBricksApp/entities/TAndFQuestionnaire.java
+++ src/meyerBricksApp/entities/TAndFQuestionnaire.java
@@ -1,2 +1,23 @@
-package meyerBricksApp.entities;public class TAndFQuestionnaire {
+package meyerBricksApp.entities;
+
+import meyerBricksApp.DataStore.services.QuestionService;
+
+public class TAndFQuestionnaire extends Questionnaire{
+    @Override
+    public void setQuestionInA() {
+        int count = 1;
+        for (int i = 15; i < 20; i++) {
+            super.getQuestionAs().put(count, QuestionService.getSetA()[i]);
+            count++;
+        }
+    }
+
+    @Override
+    public void setQuestionInB() {
+        int count = 1;
+        for (int i = 15; i < 20; i++) {
+            super.getQuestionBs().put(count, QuestionService.getSetB()[i]);
+            count++;
+        }
+    }
 }
