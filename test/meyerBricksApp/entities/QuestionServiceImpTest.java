7d8509578c4d4e8b9a4a67855ba36b0fe7494add
akenz1901 <michael_aka1@icloud.com>
2021-09-04 21:32:53 +0100

    Tested that question can be displayed and test for exception unit too

--- test/meyerBricksApp/entities/QuestionServiceImpTest.java
+++ test/meyerBricksApp/entities/QuestionServiceImpTest.java
@@ -1,2 +1,34 @@
-package meyerBricksApp.entities;public class QuestionServiceImpTest {
+package meyerBricksApp.entities;
+
+import meyerBricksApp.DataStore.services.QuestionService;
+import meyerBricksApp.DataStore.services.QuestionServiceImp;
+import meyerBricksApp.MeyerBriggsExceptions.MeyerBriggsAppException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuestionServiceImpTest {
+    QuestionService questionService;
+    Questionnaire question;
+    @BeforeEach
+    void setUp(){
+        questionService = new QuestionServiceImp();
+        question = new EAndIQuestionnaire();
+    }
+
+    @AfterEach
+    void tearDown(){}
+
+    @Test
+    void displayQuestionAInE_andIQuestionnaire(){
+        assertNotNull(questionService.displayQuestionA(question, 1));
+
+        assertEquals("more outgoing, think out loud", questionService.displayQuestionA(question, 5));
+    }
+    @Test
+    void displayQuestionAThrowAnException(){
+        assertThrows(MeyerBriggsAppException.class, () -> questionService.displayQuestionA(question, 6));
+    }
 }
