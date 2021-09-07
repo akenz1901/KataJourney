863b8ab3e096c234808ed3e04c28f5ab29474b3f
akenz1901 <michael_aka1@icloud.com>
2021-09-07 23:28:28 +0100

    Qustionnaire inevvest

--- test/meyerBricksApp/entities/QuestionServiceImpTest.java
+++ test/meyerBricksApp/entities/QuestionServiceImpTest.java
@@ -10,12 +10,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class QuestionServiceImpTest {
-    QuestionService questionService;
-    Questionnaire question;
+    private QuestionService questionService;
+    private Questionnaire question;
+    private SAndNQuestionnaire sAndNQuestionnaire;
     @BeforeEach
     void setUp(){
         questionService = new QuestionServiceImp();
         question = new EAndIQuestionnaire();
+        sAndNQuestionnaire = new SAndNQuestionnaire();
     }
 
     @AfterEach
@@ -24,11 +26,20 @@ public class QuestionServiceImpTest {
     @Test
     void displayQuestionAInE_andIQuestionnaire(){
         assertNotNull(questionService.displayQuestionA(question, 1));
-
         assertEquals("more outgoing, think out loud", questionService.displayQuestionA(question, 5));
+
+        assertEquals("Conserve energy, enjoy one-on-one", questionService.displayQuestionB(question, 1));
+
+
     }
     @Test
     void displayQuestionAThrowAnException(){
         assertThrows(MeyerBriggsAppException.class, () -> questionService.displayQuestionA(question, 6));
+    }
+    @Test
+    void displayQuestionAInS_andNQuestionnaire(){
+        assertEquals("candid, straight forward, frank", questionService.displayQuestionA(sAndNQuestionnaire, 2));
+
+        assertEquals("unplanned, spontaneous", questionService.displayQuestionB(sAndNQuestionnaire, 3));
     }
 }
