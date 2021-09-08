8f39db18fef04f25955dcc84ed7b418f66efb2a5
akenz1901 <michael_aka1@icloud.com>
2021-09-08 21:04:55 +0100

    All questions in every questionnaire can be diplayed

--- test/meyerBricksApp/entities/QuestionServiceImpTest.java
+++ test/meyerBricksApp/entities/QuestionServiceImpTest.java
@@ -13,18 +13,25 @@ public class QuestionServiceImpTest {
     private QuestionService questionService;
     private Questionnaire question;
     private SAndNQuestionnaire sAndNQuestionnaire;
+    private JAndPQuestionnaire jAndPQuestionnaire;
+    private TAndFQuestionnaire tAndFQuestionnaire;
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         questionService = new QuestionServiceImp();
         question = new EAndIQuestionnaire();
         sAndNQuestionnaire = new SAndNQuestionnaire();
+        jAndPQuestionnaire = new JAndPQuestionnaire();
+        tAndFQuestionnaire = new TAndFQuestionnaire();
+
     }
 
     @AfterEach
-    void tearDown(){}
+    void tearDown() {
+    }
 
     @Test
-    void displayQuestionAInE_andIQuestionnaire(){
+    void displayQuestionAInE_andIQuestionnaire() {
         assertNotNull(questionService.displayQuestionA(question, 1));
         assertEquals("more outgoing, think out loud", questionService.displayQuestionA(question, 5));
 
@@ -32,14 +39,29 @@ public class QuestionServiceImpTest {
 
 
     }
+
     @Test
-    void displayQuestionAThrowAnException(){
+    void displayQuestionA_andBThrowAnException() {
         assertThrows(MeyerBriggsAppException.class, () -> questionService.displayQuestionA(question, 6));
     }
+
     @Test
-    void displayQuestionAInS_andNQuestionnaire(){
+    void displayQuestionA_andBInS_andNQuestionnaire() {
         assertEquals("candid, straight forward, frank", questionService.displayQuestionA(sAndNQuestionnaire, 2));
 
         assertEquals("unplanned, spontaneous", questionService.displayQuestionB(sAndNQuestionnaire, 3));
+    }
+
+    @Test
+    void displayQuestionA_andBInJ_andPQuestionnaire() {
+        assertEquals("firm, tend to criticize, hold the line", questionService.displayQuestionA(jAndPQuestionnaire, 1));
+
+        assertEquals("tender-hearted, merciful", questionService.displayQuestionB(jAndPQuestionnaire, 5));
+    }
+    @Test
+    void displayQuestionA_andBInT_andFQuestionnaire() {
+        assertEquals("facts, things, 'what is'", questionService.displayQuestionA(tAndFQuestionnaire, 3));
+
+        assertEquals("sensitive, people-oriented, compassionate", questionService.displayQuestionB(tAndFQuestionnaire, 4));
     }
 }
