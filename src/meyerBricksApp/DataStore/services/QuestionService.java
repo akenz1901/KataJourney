7f5e68012b17c57617e8b854d2b4de65f3762a1d
akenz1901 <michael_aka1@icloud.com>
2021-09-09 23:52:42 +0100

    New behaviour now added for confirming choice

--- src/meyerBricksApp/DataStore/services/QuestionService.java
+++ src/meyerBricksApp/DataStore/services/QuestionService.java
@@ -1,9 +1,13 @@
 package meyerBricksApp.DataStore.services;
 
+import meyerBricksApp.DataStore.ChoiceType;
+import meyerBricksApp.entities.Aspirant;
 import meyerBricksApp.entities.Questionnaire;
 
 public interface QuestionService {
     String displayQuestionA(Questionnaire questionnaire, int question);
 
-    String displayQuestionB(Questionnaire questionnaire, int i);
+    String displayQuestionB(Questionnaire questionnaire, int questionNumber);
+
+    int selectChoice(ChoiceType choice, Questionnaire questionnaire);
 }
