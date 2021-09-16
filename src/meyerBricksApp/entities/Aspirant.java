fb9b552b0ae8ffd0da602594f643dcf789c7c775
akenz1901 <michael_aka1@icloud.com>
2021-09-16 22:28:25 +0100

    Aspirant stores scores

--- src/meyerBricksApp/entities/Aspirant.java
+++ src/meyerBricksApp/entities/Aspirant.java
@@ -1,2 +1,75 @@
-package meyerBricksApp.entities;public class Aspirant {
+package meyerBricksApp.entities;
+public class Aspirant {
+    int extrovertScore;
+    int introvertScore;
+    int sensingScore;
+    int intuitionScore;
+    int thinkingScore;
+    int feelingScore;
+    int judgingScore;
+    int perceivingScore;
+
+    public int getExtrovertScore() {
+        return extrovertScore;
+    }
+
+    public void addExtrovertScore() {
+        this.extrovertScore++;
+    }
+
+    public int getIntrovertScore() {
+        return introvertScore;
+    }
+
+    public void addIntrovertScore() {
+        this.introvertScore++;
+    }
+
+    public int getSensingScore() {
+        return sensingScore;
+    }
+
+    public void addSensingScore() {
+        this.sensingScore++;
+    }
+
+    public int getIntuitionScore() {
+        return intuitionScore;
+    }
+
+    public void addIntuitionScore() {
+        this.intuitionScore++;
+    }
+
+    public int getThinkingScore() {
+        return thinkingScore;
+    }
+
+    public void addThinkingScore() {
+        this.thinkingScore++;
+    }
+
+    public int getFeelingScore() {
+        return feelingScore;
+    }
+
+    public void addFeelingScore() {
+        this.feelingScore++;
+    }
+
+    public int getJudgingScore() {
+        return judgingScore;
+    }
+
+    public void addJudgingScore() {
+        this.judgingScore++;
+    }
+
+    public int getPerceivingScore() {
+        return perceivingScore;
+    }
+
+    public void addPerceivingScore() {
+        this.perceivingScore++;
+    }
 }
