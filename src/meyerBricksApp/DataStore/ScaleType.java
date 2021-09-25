ca964b8bcf3fb5756b057d50bf7d58da5ff03e29
akenz1901 <michael_aka1@icloud.com>
2021-09-25 23:04:47 +0100

    ScalaType

--- src/meyerBricksApp/DataStore/ScaleType.java
+++ src/meyerBricksApp/DataStore/ScaleType.java
@@ -1,7 +1,14 @@
-package meyerBricksApp.DataStore.services;
+package meyerBricksApp.DataStore;
 
 public enum ScaleType {
-    E("Extrovert"), I("Introvert"), S("Sensing"), N("Intuition"), T("Thinking"), F("Feeling"), J("Judging"), P("Perceiving");
+    E("Extrovert"),
+    I("Introvert"),
+    S("Sensing"),
+    N("Intuition"),
+    T("Thinking"),
+    F("Feeling"),
+    J("Judging"),
+    P("Perceiving");
     private final String meaning;
     ScaleType(String meaning){
         this.meaning = meaning;
