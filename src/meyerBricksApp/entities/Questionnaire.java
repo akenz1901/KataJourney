3d5a598db38e554596fa446e3c5a02d40c275707
akenz1901 <michael_aka1@icloud.com>
2021-10-01 21:52:31 +0100

    I made Questionnare stores the question number being preview

--- src/meyerBricksApp/entities/Questionnaire.java
+++ src/meyerBricksApp/entities/Questionnaire.java
@@ -2,27 +2,30 @@ package meyerBricksApp.entities;
 
 import meyerBricksApp.DataStore.ChoiceType;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public abstract class Questionnaire {
     private final Map<Integer, String> questionAs = new HashMap<>(5);
     private final Map<Integer, String> questionBs = new HashMap<>(5);
-    private ChoiceType preference;
     protected static int IDNUMBER;
+    protected List<Integer> questionTrack = new ArrayList<>(5);
+
+    public List<Integer> getQuestionTrack() {
+        return questionTrack;
+    }
+
+    private void setQuestionTrack(List<Integer> questionTrack) {
+        this.questionTrack = questionTrack;
+    }
 
     protected abstract  void storeQuestionInA();
     protected abstract void storeQuestionInB();
 
     public abstract int getIDNumber();
 
-    protected ChoiceType getPreference() {
-        return preference;
-    }
-
-    protected void setPreference(ChoiceType preference) {
-        this.preference = preference;
-    }
 
     public Map<Integer, String> getQuestionAs() {
         return questionAs;
