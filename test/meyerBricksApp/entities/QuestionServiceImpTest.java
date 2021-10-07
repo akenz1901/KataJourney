c0d295bad7853dd360fe177a978847b8a6708b95
akenz1901 <michael_aka1@icloud.com>
2021-10-07 22:42:49 +0100

    Test Coverage added

--- test/meyerBricksApp/entities/QuestionServiceImpTest.java
+++ test/meyerBricksApp/entities/QuestionServiceImpTest.java
@@ -15,39 +15,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuestionServiceImpTest {
     private QuestionService questionService;
-    private Questionnaire question;
+    private EAndIQuestionnaire eAndIQuestionnaire;
     private SAndNQuestionnaire sAndNQuestionnaire;
     private JAndPQuestionnaire jAndPQuestionnaire;
     private TAndFQuestionnaire tAndFQuestionnaire;
+    private Aspirant mercy;
 
     @BeforeEach
     void setUp() {
         questionService = new QuestionServiceImp();
-        question = new EAndIQuestionnaire();
+        eAndIQuestionnaire = new EAndIQuestionnaire();
         sAndNQuestionnaire = new SAndNQuestionnaire();
         jAndPQuestionnaire = new JAndPQuestionnaire();
         tAndFQuestionnaire = new TAndFQuestionnaire();
+        mercy = new Aspirant("Akenz", "Michael", LocalDate.of(1995, 4, 24));
 
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown(){
         QuestionnaireService.resetControl();
     }
 
     @Test
     void displayQuestionAInE_andIQuestionnaire() {
-        assertNotNull(questionService.displayQuestionA(question, 1));
-        assertEquals("more outgoing, think out loud", questionService.displayQuestionA(question, 5));
+        assertNotNull(questionService.displayQuestionA(eAndIQuestionnaire, 1));
+        assertEquals("more outgoing, think out loud", questionService.displayQuestionA(eAndIQuestionnaire, 5));
 
-        assertEquals("conserve energy, enjoy one-on-one", questionService.displayQuestionB(question, 1));
+        assertEquals("conserve energy, enjoy one-on-one", questionService.displayQuestionB(eAndIQuestionnaire, 1));
 
 
     }
 
     @Test
     void displayQuestionA_andBThrowAnException() {
-        assertThrows(MeyerBriggsAppException.class, () -> questionService.displayQuestionA(question, 6));
+        assertThrows(MeyerBriggsAppException.class, () -> questionService.displayQuestionA(eAndIQuestionnaire, 6));
     }
 
     @Test
@@ -72,38 +74,88 @@ public class QuestionServiceImpTest {
     @Test
     void displayQuestionThrowsQuestionAlreadyDisplayExceptionInQuestionA(){
         assertEquals("facts, things, 'what is'", questionService.displayQuestionA(tAndFQuestionnaire, 3));
-
         assertThrows( MeyerBriggsAppException.class, ()-> questionService.displayQuestionA(tAndFQuestionnaire, 3));
 
     }
     @Test
-    void selectChoice(){
-        assertEquals("expend energy, enjoy groups", questionService.displayQuestionA(question, 1));
-        assertEquals("conserve energy, enjoy one-on-one", questionService.displayQuestionB(question, 1));
-        Aspirant mercy = new Aspirant("Akenz", "Michael", LocalDate.of(1995, 4, 24));
-        assertEquals(1, questionService.selectChoice(ChoiceType.B, mercy));
+    void confirmChoiceSelectChoice(){
+        assertEquals("expend energy, enjoy groups", questionService.displayQuestionA(eAndIQuestionnaire, 1));
+        assertEquals("conserve energy, enjoy one-on-one", questionService.displayQuestionB(eAndIQuestionnaire, 1));
+        eAndIQuestionnaire.setChoice(ChoiceType.B);
+        assertEquals(ChoiceType.B, questionService.confirmChoiceWasSelected(eAndIQuestionnaire));
     }
     @Test
-    void selectChoiceMoreThanOneTime(){
-        assertEquals("expend energy, enjoy groups", questionService.displayQuestionA(question, 1));
-        assertEquals("conserve energy, enjoy one-on-one", questionService.displayQuestionB(question, 1));
-        Aspirant mercy = new Aspirant("Akenz", "Michael", LocalDate.of(1995, 4, 24));
-        assertEquals(1, questionService.selectChoice(ChoiceType.B, mercy));
+    void selectChoiceForE_andIQuestionnaire(){
+        assertEquals("expend energy, enjoy groups", questionService.displayQuestionA(eAndIQuestionnaire, 1));
+        assertEquals("conserve energy, enjoy one-on-one", questionService.displayQuestionB(eAndIQuestionnaire, 1));
+        eAndIQuestionnaire.setChoice(ChoiceType.B);
 
-        assertEquals("interpret literally", questionService.displayQuestionA(question, 2));
-        assertEquals("look for meaning and possibilities", questionService.displayQuestionB(question, 2));
-        assertEquals(2, questionService.selectChoice(ChoiceType.B, mercy));
+        assertEquals(ChoiceType.B, questionService.confirmChoiceWasSelected(eAndIQuestionnaire));
+        assertEquals(1, questionService.scoreCandidate(eAndIQuestionnaire, mercy));
 
-        assertEquals("logical, thinking, questioning", questionService.displayQuestionA(question, 3));
-        assertEquals("empathetic, feeling, accommodating", questionService.displayQuestionB(question, 3));
-        assertEquals(1, questionService.selectChoice(ChoiceType.A, mercy));
+        assertEquals("interpret literally", questionService.displayQuestionA(eAndIQuestionnaire, 2));
+        assertEquals("look for meaning and possibilities", questionService.displayQuestionB(eAndIQuestionnaire, 2));
+        eAndIQuestionnaire.setChoice(ChoiceType.B);
 
-        assertEquals("organized, orderly", questionService.displayQuestionA(question, 4));
-        assertEquals("flexible, adaptable", questionService.displayQuestionB(question, 4));
-        assertEquals(3, questionService.selectChoice(ChoiceType.B, mercy));
+        assertEquals(ChoiceType.B, questionService.confirmChoiceWasSelected(eAndIQuestionnaire));
+        assertEquals(2, questionService.scoreCandidate(eAndIQuestionnaire, mercy));
 
-        assertEquals("more outgoing, think out loud", questionService.displayQuestionA(question, 5));
-        assertEquals("more reserved, think to yourself", questionService.displayQuestionB(question, 5));
-        assertEquals(2, questionService.selectChoice(ChoiceType.A, mercy));
+        assertEquals("logical, thinking, questioning", questionService.displayQuestionA(eAndIQuestionnaire, 3));
+        assertEquals("empathetic, feeling, accommodating", questionService.displayQuestionB(eAndIQuestionnaire, 3));
+        eAndIQuestionnaire.setChoice(ChoiceType.A);
+
+        assertEquals(ChoiceType.A, questionService.confirmChoiceWasSelected(eAndIQuestionnaire));
+        assertEquals(1, questionService.scoreCandidate(eAndIQuestionnaire, mercy));
+
+        assertEquals("organized, orderly", questionService.displayQuestionA(eAndIQuestionnaire, 4));
+        assertEquals("flexible, adaptable", questionService.displayQuestionB(eAndIQuestionnaire, 4));
+        eAndIQuestionnaire.setChoice(ChoiceType.B);
+
+        assertEquals(ChoiceType.B, questionService.confirmChoiceWasSelected(eAndIQuestionnaire));
+        assertEquals(3, questionService.scoreCandidate(eAndIQuestionnaire, mercy));
+
+        assertEquals("more outgoing, think out loud", questionService.displayQuestionA(eAndIQuestionnaire, 5));
+        assertEquals("more reserved, think to yourself", questionService.displayQuestionB(eAndIQuestionnaire, 5));
+        eAndIQuestionnaire.setChoice(ChoiceType.A);
+
+        assertEquals(ChoiceType.A, questionService.confirmChoiceWasSelected(eAndIQuestionnaire));
+        assertEquals(2, questionService.scoreCandidate(eAndIQuestionnaire, mercy));
+    }
+    @Test
+    void selectChoiceForS_andNQuestionnaire(){
+        assertEquals("practical, realistic, experiential", questionService.displayQuestionA(sAndNQuestionnaire, 1));
+        assertEquals("imaginative, innovative, theoretical", questionService.displayQuestionB(sAndNQuestionnaire, 1));
+        sAndNQuestionnaire.setChoice(ChoiceType.B);
+
+        assertEquals(ChoiceType.B, questionService.confirmChoiceWasSelected(sAndNQuestionnaire));
+        assertEquals(1, questionService.scoreCandidate(sAndNQuestionnaire, mercy));
+
+        assertEquals("candid, straight forward, frank", questionService.displayQuestionA(sAndNQuestionnaire, 2));
+        assertEquals("tactful kind, encouraging", questionService.displayQuestionB(sAndNQuestionnaire, 2));
+        sAndNQuestionnaire.setChoice(ChoiceType.B);
+
+        assertEquals(ChoiceType.B, questionService.confirmChoiceWasSelected(sAndNQuestionnaire));
+        assertEquals(2, questionService.scoreCandidate(sAndNQuestionnaire, mercy));
+
+        assertEquals("plan, schedule", questionService.displayQuestionA(sAndNQuestionnaire, 3));
+        assertEquals("unplanned, spontaneous", questionService.displayQuestionB(sAndNQuestionnaire, 3));
+        sAndNQuestionnaire.setChoice(ChoiceType.A);
+
+        assertEquals(ChoiceType.A, questionService.confirmChoiceWasSelected(sAndNQuestionnaire));
+        assertEquals(1, questionService.scoreCandidate(sAndNQuestionnaire, mercy));
+
+        assertEquals("seek many tasks, public activities, interaction with others", questionService.displayQuestionA(sAndNQuestionnaire, 4));
+        assertEquals("seek private, solitary activities with quiet to concentrate", questionService.displayQuestionB(sAndNQuestionnaire, 4));
+        sAndNQuestionnaire.setChoice(ChoiceType.B);
+
+        assertEquals(ChoiceType.B, questionService.confirmChoiceWasSelected(sAndNQuestionnaire));
+        assertEquals(3, questionService.scoreCandidate(sAndNQuestionnaire, mercy));
+
+        assertEquals("standard usual conventional", questionService.displayQuestionA(sAndNQuestionnaire, 5));
+        assertEquals("different, novel, unique", questionService.displayQuestionB(sAndNQuestionnaire, 5));
+        sAndNQuestionnaire.setChoice(ChoiceType.A);
+
+        assertEquals(ChoiceType.A, questionService.confirmChoiceWasSelected(sAndNQuestionnaire));
+        assertEquals(2, questionService.scoreCandidate(sAndNQuestionnaire, mercy));
     }
 }
