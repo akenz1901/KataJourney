8c7029b65d4a4034b4495b4fd1bbd5707d593c5d
= <=>
2022-11-04 16:50:49 +0100

    Challenges

--- src/algorithms/BracketMerge.java
+++ src/algorithms/BracketMerge.java
@@ -1,4 +1,47 @@
 package algorithms;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BracketMerge {
+
+    public String[] mergeBrackets(int num) {
+        List<String> determinedBrackets = new ArrayList<>();
+        String open = "(";
+        String close = ")";
+        StringBuilder finalResult = new StringBuilder();
+        boolean constraint = 1 >= num & num <= 8;
+
+        if(constraint){
+            switch (num){
+                case 1:
+                    return new String[]{open + close};
+                case 2:
+                    for (int i = 1; i <= num; i++) {
+                        for (int j = 0; j < num; j++) {
+                            finalResult.append(open).append(close);
+                            determinedBrackets.add(finalResult.toString());
+                        }
+                        for (int k = 0; k < num-1; k++) {
+                            finalResult = new StringBuilder();
+                            finalResult.append(close)
+                                    .append(close)
+                                    .append(open)
+                                    .append(open);
+                        }
+                    }
+                    return determinedBrackets.toArray(new String[0]);
+            }
+        }
+
+        return new String[]{};
+    }
+
+    public static void main(String... args){
+        BracketMerge bracketMerge = new BracketMerge();
+
+        System.out.println(Arrays.toString(bracketMerge.mergeBrackets(2)));
+    }
 }
---
+++ src/coderbyteAlgorithm/AgeCount.java
@@ -0,0 +1,41 @@
+package coderbyteAlgorithm;
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+class AgeCount {
+    public static void main (String[] args) {
+        String url = "https://coderbyte.com/api/challenges/json/age-counting";
+
+
+        try{
+            URL urlOb = new URL(url);
+
+            InputStream conn = urlOb.openStream();
+            InputStreamReader isR = new InputStreamReader(conn);
+            BufferedReader reader = new BufferedReader(isR);
+            StringBuilder json = new StringBuilder();
+
+            int c;
+            while((c = reader.read()) != -1){
+                json.append((char) c);
+            }
+            System.out.println(json);
+
+            String[] arrJson = json.toString().split("'{'");
+            System.out.println(arrJson[0]);
+//            int count = 0;
+//            for(String field:arrJson){
+//                String[] each = field.split("=");
+//                if(each[0].strip().equals("age") && Integer.parseInt(each[1].strip())>=50){
+//                    count++;
+//                    System.out.println(count);
+//                }
+//            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
