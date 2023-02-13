1c04f5040523f464ab3f603cf713ecd5cb455bef
akenz1901 <=>
2023-02-13 18:43:30 +0100

    Testing Thread class

--- src/tipCalculator/TipCalculatorApplication.java
+++ src/tipCalculator/TipCalculatorApplication.java
@@ -2,38 +2,48 @@ package tipCalculator;
 
 import java.util.Scanner;
 
-public class TipCalculatorApplication {
+public class TipCalculatorApplication implements Runnable{
 
     public static void main(String... args) {
 
-        Scanner input = new Scanner(System.in);
+    Thread runner = new Thread(new TipCalculatorApplication());
+    runner.start();
+    }
 
-        TipCalculator tipCalculator = new TipCalculator();
+    public void run() {
+        while(true) {
+            try {
+                Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter amount: ");
+                TipCalculator tipCalculator = new TipCalculator();
 
-        double enteredAmount = input.nextDouble();
+                System.out.print("Enter amount: ");
 
-        tipCalculator.setAmount(enteredAmount);
+                double enteredAmount = input.nextDouble();
 
-        final int press18PerButton = 18;
-        final int press20PerButton = 20;
+                tipCalculator.setAmount(enteredAmount);
 
-        String displayedMessage = String.format("""
-                    How many percentage do you want to calculate %.2f with?
-                    Enter one of the followings:
-                    
-                    1. 18
-                    2. 20
-                """, enteredAmount);
-        System.out.println(displayedMessage);
+                final int press18PerButton = 18;
+                final int press20PerButton = 20;
 
-        int pressButton = input.nextInt();
-        switch (pressButton){
-            case press18PerButton -> System.out.println(tipCalculator.calculateAmountWith18Per());
-            case press20PerButton -> System.out.println(tipCalculator.calculateAmountWith20Per());
-            default->  System.out.println("Kindly enter one of the given options");
+                String displayedMessage = String.format("""
+                            How many percentage do you want to calculate %.2f with?
+                            Enter one of the followings:
+                            
+                            1. 18
+                            2. 20
+                        """, enteredAmount);
+                System.out.println(displayedMessage);
+
+                int pressButton = input.nextInt();
+                switch (pressButton) {
+                    case press18PerButton -> System.out.println(tipCalculator.calculateAmountWith18Per());
+                    case press20PerButton -> System.out.println(tipCalculator.calculateAmountWith20Per());
+                    default -> System.out.println("Kindly enter one of the given options");
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
-
     }
 }
