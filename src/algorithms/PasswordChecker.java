905073fc7f5c6dde9dadf907034fdde68e10ea65
akenz1901 <=>
2023-02-22 21:51:57 +0100

    Hackerank Algorithm

---
+++ src/algorithms/PasswordChecker.java
@@ -0,0 +1,80 @@
+package algorithms;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /* * Complete the 'getPasswordStrength' function below.
+     * * The function is expected to return a STRING_ARRAY.
+     * * The function accepts following parameters:
+     * *  1. STRING_ARRAY passwords
+     * *  2. STRING_ARRAY common_words */
+    public static List<String> getPasswordStrength(List<String> passwords, List<String> common_words) {
+        List<String> outputs = new ArrayList();
+        final String WEAK = "weak";
+        final String STRONG = "strong";
+        int cm = 0;
+        for(String word : passwords){
+            if(word.length() < 6){ cm = 1; }
+            else if(word.equals(word.toLowerCase()) || word.equals(word.toUpperCase())){
+                cm = 1;
+            }else if(containsOnlyDigits(word)){ cm = 1; }
+
+            for(String cword:common_words){
+                if(word.contains(cword)){ cm = 1;
+                }else if(cword.endsWith(word)){
+                    cm = 1;
+                }
+            } if(cm == 1){
+                outputs.add(WEAK);
+            }else{
+                outputs.add(STRONG);
+            } cm = 0;
+        }
+        return outputs;
+    }
+
+    public static boolean containsOnlyDigits(String password){
+        int j = 0;
+        for(int i = 0; i < password.length(); i++){
+            if(Character.isDigit(password.charAt(i))){
+                j++;
+            }
+        }
+        if(j == password.length()){ return true;
+        }
+        return false;
+    } }
+
+public class PasswordChecker {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        int passwordsCount = Integer.parseInt(bufferedReader.readLine().trim());
+        List<String> passwords = IntStream.range(0, passwordsCount).mapToObj(i -> {
+            try { return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            } }) .collect(toList());
+        int common_wordsCount = Integer.parseInt(bufferedReader.readLine().trim());
+        List<String> common_words = IntStream.range(0, common_wordsCount).mapToObj(i -> {
+            try { return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }) .collect(toList());
+        List<String> result = Result.getPasswordStrength(passwords, common_words);
+        bufferedWriter.write( result.stream() .collect(joining("\n")) + "\n" );
+        bufferedReader.close(); bufferedWriter.close();
+    }
+}
--- src/chapterFour/KataTest.java
+++ src/chapterFour/KataTest.java
@@ -1,15 +1,28 @@
 package chapterFour;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class KataTest {
         public static void main(String... args) {
-            Kata kata = new Kata();
-            System.out.println("Check if it's Even " + kata.isEven(2));
-            System.out.println("Grade is " + kata.calculateGrade(4));
-            System.out.println("The average is " + kata.calculateAverage());
-            System.out.println("Is It Prime Number ? " + kata.isPrime(7));
-            System.out.println(Kata.copiesPrice());
-            System.out.println(kata.checkTheFactorsOfANumber());
-            System.out.println(kata.arithmeticNumbers(29,3));
+//            Kata kata = new Kata();
+//            System.out.println("Check if it's Even " + kata.isEven(2));
+//            System.out.println("Grade is " + kata.calculateGrade(4));
+//            System.out.println("The average is " + kata.calculateAverage());
+//            System.out.println("Is It Prime Number ? " + kata.isPrime(7));
+//            System.out.println(Kata.copiesPrice());
+//            System.out.println(kata.checkTheFactorsOfANumber());
+//            System.out.println(kata.arithmeticNumbers(29,3));
+
+            String [] names = {"Jide", "Mercy", "Ola", "Temio"};
+            String [] reversed_names = new String[names.length];
+             int count = 0;
+
+            for(int i = names.length-1; i >= 0; i--){
+                reversed_names[count] = names[i];
+                count++;
+            }
+            System.out.println(Arrays.toString(reversed_names));
 
         }
     }
